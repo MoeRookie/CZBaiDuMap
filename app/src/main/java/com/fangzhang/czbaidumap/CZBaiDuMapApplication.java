c32df6d4b223b73ff761f7db0cc685e56feaed22
MoeRookie <18401430395@163.com>
2017-11-17 14:46:02 +0800

    配置开发环境，初始化并显示地图 .

---
+++ app/src/main/java/com/fangzhang/czbaidumap/CZBaiDuMapApplication.java
@@ -0,0 +1,18 @@
+package com.fangzhang.czbaidumap;
+
+import android.app.Application;
+
+import com.baidu.mapapi.SDKInitializer;
+
+/**
+ * Created by Administrator on 2017/11/17.
+ */
+
+public class CZBaiDuMapApplication extends Application {
+    @Override
+    public void onCreate() {
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+    }
+}
