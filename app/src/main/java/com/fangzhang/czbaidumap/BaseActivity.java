9804ea2e50f6ff2c2988abcaa093709431f0c09e
MoeRookie <18401430395@163.com>
2017-11-20 16:23:59 +0800

    抽取BaseActivity类以消除代码重复 .

--- app/src/main/java/com/fangzhang/czbaidumap/BaseActivity.java
+++ app/src/main/java/com/fangzhang/czbaidumap/BaseActivity.java
@@ -1,14 +1,84 @@
 package com.fangzhang.czbaidumap;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.Toast;
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.UiSettings;
+import com.baidu.mapapi.model.LatLng;
 
 /**
  * Created by Administrator on 2017/11/17.
  */
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
+
+    private static final String TAG = "BaseActivity";
+
+    /** 黑马坐标（北京市海淀区东北旺南路45号）*/
+    protected LatLng hmPos = new LatLng(40.050513, 116.30361);
+    /** 传智坐标 */
+    protected LatLng czPos = new LatLng(40.065817,116.349902);
+    /** 天安门坐标 */
+    protected LatLng tamPos = new LatLng(39.915112,116.403963);
+
+    MapView mMapView;
+    protected BaiduMap mapController;
+
+    @Override
+    protected final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = findViewById(R.id.bmapView);
+        mapController = mMapView.getMap();
+//    6.获取地图Ui控制器：隐藏指南针
+//      1.隐藏缩放按钮、比例尺(默认显示缩放按钮、比例尺)
+//        mMapView.showScaleControl(false);
+//        mMapView.showScaleControl(false);
+//      2.获取最小（3）、最大缩放级别（20）
+        float maxZoomLevel = mapController.getMaxZoomLevel();
+        float minZoomLevel = mapController.getMinZoomLevel();
+        Log.e(TAG, "onCreate,maxZoomLevel = " + maxZoomLevel + ",minZoomLevel = " + minZoomLevel);
+//      3.设置地图中心点为黑马
+        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(hmPos);
+        mapController.setMapStatus(mapStatusUpdate);
+//      4.设置地图缩放为15
+        mapStatusUpdate = MapStatusUpdateFactory.zoomTo(15);
+        mapController.setMapStatus(mapStatusUpdate);
+//      6.获取地图Ui控制器：隐藏指南针
+        UiSettings uiSettings = mapController.getUiSettings();
+        uiSettings.setCompassEnabled(false);
+        init();
+    }
+
+    protected abstract void init();
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
     /**
      * 在屏幕中央显示一个Toast
      * @param text 要显示的内容
--- app/src/main/java/com/fangzhang/czbaidumap/MainActivity.java
+++ app/src/main/java/com/fangzhang/czbaidumap/MainActivity.java
@@ -20,41 +20,12 @@ import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
-
-    /** 黑马坐标（北京市海淀区东北旺南路45号）*/
-    protected LatLng hmPos = new LatLng(40.050513, 116.30361);
-    /** 传智坐标 */
-    protected LatLng czPos = new LatLng(40.065817,116.349902);
-    /** 天安门坐标 */
-    protected LatLng tamPos = new LatLng(39.915112,116.403963);
-
-    MapView mMapView;
     private BroadcastReceiver receiver;
-    private BaiduMap mapController;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void init() {
         // 注册SDK的广播接收者监听
         registerSDKCheckReceiver();
-        setContentView(R.layout.activity_main);
-        //获取地图控件引用
-        mMapView = findViewById(R.id.bmapView);
-        mapController = mMapView.getMap();
-//    6.	获取地图Ui控制器：隐藏指南针
-//      1.隐藏缩放按钮、比例尺(默认显示缩放按钮、比例尺)
-//        mMapView.showScaleControl(false);
-//        mMapView.showScaleControl(false);
-//      2.获取最小（3）、最大缩放级别（20）
-        float maxZoomLevel = mapController.getMaxZoomLevel();
-        float minZoomLevel = mapController.getMinZoomLevel();
-        Log.e(TAG, "onCreate,maxZoomLevel = " + maxZoomLevel + ",minZoomLevel = " + minZoomLevel);
-//      3.设置地图中心点为黑马
-        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(hmPos);
-        mapController.setMapStatus(mapStatusUpdate);
-//      4.设置地图缩放为15
-        mapStatusUpdate = MapStatusUpdateFactory.zoomTo(15);
-        mapController.setMapStatus(mapStatusUpdate);
     }
 
     private void registerSDKCheckReceiver() {
@@ -75,6 +46,12 @@ public class MainActivity extends BaseActivity {
         // 监听百度地图sdk的key是否正确
         filter.addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR);
         registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 
     @Override
@@ -114,25 +91,5 @@ public class MainActivity extends BaseActivity {
         }
         mapController.setMapStatus(status);
         return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-        unregisterReceiver(receiver);
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
     }
 }
